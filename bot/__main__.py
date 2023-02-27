116534a46fec2cb86fbfbb56ff460a1505e41592
developerssh <119683709+developerssh@users.noreply.github.com>
2023-02-27 20:20:43 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -110,9 +110,9 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     buttons.buildbutton(
-        "Owner😃", "@riot8"
+        "Repo😃", "https://github.com/kshitij
     )
-    buttons.buildbutton("Developer🖥", "https://t.me/chochonder")
+    buttons.buildbutton("Report Group🖥", "https://t.me/chochonder")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f"""
